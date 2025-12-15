951167c32ac98740c173ed61535318b6c77f081f
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2025-12-15 12:11:04 -0500

    Add files via upload

--- 11.py
+++ 11.py
@@ -1,4 +1,4 @@
-from functools import cache
+from functools import lru_cache
 
 D = dict()  # device -> set of connected devices
 for line in open("11.input").readlines():
@@ -6,20 +6,28 @@ for line in open("11.input").readlines():
     D[L] = set(R.split())
 
 
-@cache
+@lru_cache
 def numpaths(start, finish, avoid):
     global D
     if start == finish:
         return 1
-    if start == avoid or start not in D:
+    if start in avoid or start not in D:
         return 0
     return sum(numpaths(n, finish, avoid) for n in D[start])
 
 
-print(f"Part 1: {numpaths('you', 'out', '')}")
+print(f"Part 1: {numpaths('you', 'out', ())}")
 print(
     f"Part 2: {
-        numpaths('svr', 'fft', 'dac') * numpaths('fft', 'dac', 'out') * numpaths('dac', 'out', 'fft')
-        + numpaths('svr', 'dac', 'fft') * numpaths('dac', 'fft', 'out') * numpaths('fft', 'out', 'dac')
+        (
+            numpaths('svr', 'fft', ('dac', 'out'))
+            * numpaths('fft', 'dac', ('svr', 'out'))
+            * numpaths('dac', 'out', ('fft', 'svr'))
+        )
+        + (
+            numpaths('svr', 'dac', ('fft', 'out'))
+            * numpaths('dac', 'fft', ('svr', 'out'))
+            * numpaths('fft', 'out', ('dac', 'svr'))
+        )
     }"
 )
